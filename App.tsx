aac6115fbd2917ff194da7cc194775f73eb58d5f
Anshika-123-tech <anshikakashyap724@gmail.com>
2026-03-03 18:49:01 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,40 @@
+import "./global.css";
+
+import { Toaster } from "@/components/ui/toaster";
+import { createRoot } from "react-dom/client";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import RoleSelection from "./pages/RoleSelection";
+import EmployeeDashboard from "./pages/EmployeeDashboard";
+import CustomerServiceDashboard from "./pages/CustomerServiceDashboard";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/role-selection" element={<RoleSelection />} />
+          <Route path="/cs-dashboard" element={<CustomerServiceDashboard />} />
+          <Route path="/employee-dashboard" element={<EmployeeDashboard />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+createRoot(document.getElementById("root")!).render(<App />);
